74e11702e95521b95aa9edf0d43c7e5627864353
ludo vyc <theludovyc@gmail.com>
2022-03-03 17:10:10 +0100

    camera movement

--- Script/Camera_Origin.gd
+++ Script/Camera_Origin.gd
@@ -1,8 +1,9 @@
 extends Spatial
 
-const mouse_sensitivity = 0.25
+const mouse_sensitivity_rot = 0.25
+const mouse_sensitivity_tr = 0.001
 
-var mouse_speed:float
+var mouse_speed:Vector2
 
 var mouse_have_move:bool
 
@@ -10,7 +11,7 @@ var zoom:float
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
-		mouse_speed = event.relative.x
+		mouse_speed = event.relative
 		mouse_have_move = true
 
 # Called when the node enters the scene tree for the first time.
@@ -19,8 +20,16 @@ func _ready():
 
 func _process(delta):
 	if Input.is_action_pressed("mouse_right") and mouse_have_move:
-		rotate_y(-mouse_speed * delta * mouse_sensitivity)
+		rotate_y(-mouse_speed.x * delta * mouse_sensitivity_rot)
 		mouse_have_move = false
+	
+	if Input.is_action_just_pressed("mouse_middle"):
+		Input.warp_mouse_position(get_viewport().size/2)
+		
+	if Input.is_action_pressed("mouse_middle"):
+		var mouse_lenght_from_center = get_viewport().get_mouse_position() - get_viewport().size/2
+		
+		translate(Vector3(mouse_lenght_from_center.x, 0, mouse_lenght_from_center.y) * mouse_sensitivity_tr)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
