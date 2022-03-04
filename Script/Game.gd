e3f99ba2ee69085cba0693333c25d95c01a4a350
theludovyc <theludovyc@gmail.com>
2022-03-04 17:20:41 +0100

    start generate island

--- Script/Game.gd
+++ Script/Game.gd
@@ -1,11 +1,33 @@
 extends Node
 
+var Cube_Column = preload("res://Scene/Cube_Column.tscn")
+
 var score := 0
 
 onready var gui = $GUI
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var noise = OpenSimplexNoise.new()
+	
+	# Configure
+	noise.seed = randi()
+	noise.octaves = 9
+	noise.period = 0.1
+	noise.persistence = 0
+	
+	var vec_max = Vector2(5, 5)
+	var vec_center = vec_max/2
+	
+	for i in 5:
+		for j in 5:
+			var column = Cube_Column.instance()
+			var noi = noise.get_noise_2d(i, j)
+			
+			var vec = Vector2(i, j) - vec_center
+			
+			column.translation = Vector3(5*i, noi + vec.normalized().length() , 5*j)
+			add_child(column)
 	pass # Replace with function body.
 
 
