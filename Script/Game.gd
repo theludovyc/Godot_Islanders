cc32a12364b485260d95c2e35224d1ea1977ad65
theludovyc <theludovyc@gmail.com>
2022-03-07 14:54:52 +0100

    end land generate

--- Script/Game.gd
+++ Script/Game.gd
@@ -2,12 +2,19 @@ extends Node
 
 var Cube_Column = preload("res://Scene/Cube_Column.tscn")
 
+const column_width = 5
+const column_min_height = 0.25
+const land_width = 3
+const pop_threshold = -0.2
+
 var score := 0
 
 onready var gui = $GUI
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize()
+	
 	var noise = OpenSimplexNoise.new()
 	
 	# Configure
@@ -16,20 +23,16 @@ func _ready():
 	noise.period = 0.1
 	noise.persistence = 0
 	
-	var vec_max = Vector2(11, 11)
-	var vec_center = vec_max/2
-	
-	for i in 10:
-		for j in 10:
+	for i in land_width:
+		for j in land_width:
 			var column = Cube_Column.instance()
 			var noi = noise.get_noise_2d(i, j)
 			
 			prints("Game", noi)
 			
-			var vec = Vector2(i, j) - vec_center
-			
-			column.translation = Vector3(2.5*i, noi + range_lerp(vec.length(), 0, vec_center.length(), vec_center.length(), 0), 2.5*j)
-			add_child(column)
+			if noi > pop_threshold:
+				column.translation = Vector3(column_width*i, column_min_height + noi + -pop_threshold, column_width*j)
+				add_child(column)
 	pass # Replace with function body.
 
 
