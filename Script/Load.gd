ab8b0d3c7729a86adbb0bcb14bb7c97caf2139f4
theludovyc <theludovyc@gmail.com>
2022-03-08 15:40:32 +0100

    add viking pack

--- Script/Load.gd
+++ Script/Load.gd
@@ -8,5 +8,9 @@ const packs = {"Adventure":
 			"Adventure_House_01":preload("res://Scene/Adventure/Adventure_House_01.tscn"),
 			"Adventure_House_02":preload("res://Scene/Adventure/Adventure_House_02.tscn")
 		}
+	,"Viking":
+		{
+			"Viking_Tower_01":preload("res://Scene/Viking/Viking_Tower_01.tscn")
+		}
 	}
 	
