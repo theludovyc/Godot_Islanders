31294821e106351a4acb1c6fe9d0d8e16c991708
theludovyc <theludovyc@gmail.com>
2022-03-11 16:13:02 +0100

    add building icons

--- Script/Icones.gd
+++ Script/Icones.gd
@@ -3,4 +3,8 @@ extends Spatial
 func _on_Button_pressed() -> void:
 	var building_name = $Building.get_children()[0].name
 	
-	$Camera.get_viewport().get_texture().get_data().save_png("D:/Downloads/Assets/POLYGON_Mini_Fantasy_Source_Files/IconsBuildings/" + building_name + ".png")
+	var path = "D:/Downloads/Assets/POLYGON_Mini_Fantasy_Source_Files/IconsBuildings/" + building_name + ".png"
+	
+	$Camera.get_viewport().get_texture().get_data().save_png(path)
+
+	prints("Icones", path)
